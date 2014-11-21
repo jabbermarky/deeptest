00a087d64f4ea6db6ed60e679288e487221f9066
Mark Lummus <mark@togetherbrands.com>
2014-11-21 10:05:59 -0500

    updated index page with login form and signup button

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+    username: "",
+    password: "",
+    errorMessage:"",
+    credentialError:false,
+    actions: {
+        signIn: function () {
+            console.log('signIn username '+this.get('username'));
+            if (this.username) {
+                if (this.password) {
+                    this.set('errorMessage','');
+                    this.set('credentialError', false);
+                    this.transitionToRoute('dashboard');
+                } else {
+                    this.set('credentialError', true);
+                    this.set('errorMessage', 'please enter a valid password');
+                }
+            } else {
+                this.set('credentialError', true);
+                this.set('errorMessage', 'please enter a valid username');
+            }
+        }
+    }
 });
